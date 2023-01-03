c8ed9ba469e7bd2e756d8f8f54411cbd2ef597ad
gdhdhdh1441414 <105533517+gdhdhdh1441414@users.noreply.github.com>
2023-01-03 11:29:35 +0800

    Create delete.php

---
+++ web/delete.php
@@ -0,0 +1,8 @@
+<?php
+shell_exec("rm -rf ./ysf2020/*"); 
+shell_exec("rm -rf ./ysf20202/*"); 
+shell_exec("rm -rf ./ysf20201/*"); 
+shell_exec("rm -rf ./backup/*"); 
+shell_exec("rm -rf ./backup1/*"); 
+shell_exec("rm -rf ./backup2/*"); 
+shell_exec("rm -rf ./remote*/*"); 
