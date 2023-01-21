4d82cec223a69baf156a59866c0ea8f525b2ea08
gdhdhdh1441414 <105533517+gdhdhdh1441414@users.noreply.github.com>
2023-01-21 10:43:26 +0800

    Update proxy.php

--- web/proxy.php
+++ web/proxy.php
@@ -1315,14 +1315,9 @@ else if  (strlen($result) > 6) {echo $result;}  //如果有数据输出，且没
 
 else  {   //上述条件均不满足（即无数据输出，curl_php遇到验证码确实是无数据输出），就是有验证码了
 
-
-
-
 $arr = array(
-    'https://sovj2weiosjke007php.herokuapp.com/proxy.php?url='.$url.'&ref='.$ref.'&name=tocviuwifjiaie007php&token=b52cb0e0-8120-420f-bd2a-dbaff7435c9e',
-    'https://sovj2weiosjke008php.herokuapp.com/proxy.php?url='.$url.'&ref='.$ref.'&name=tocviuwifjiaie008php&token=b52cb0e0-8120-420f-bd2a-dbaff7435c9e',
-    'https://sovj2weiosjke005php.herokuapp.com/proxy.php?url='.$url.'&ref='.$ref.'&name=tocviuwifjiaie005php&token=b52cb0e0-8120-420f-bd2a-dbaff7435c9e',
-    'https://sovj2weiosjke006php.herokuapp.com/proxy.php?url='.$url.'&ref='.$ref.'&name=tocviuwifjiaie006php&token=b52cb0e0-8120-420f-bd2a-dbaff7435c9e'
+    'https://sovj2weiosjke007php.herokuapp.com/proxy.php?url='.$url.'&ref='.$ref.'&name=pistolwayne003php&token=0e8635cf-e01e-4d5d-b778-53bb2ec48453',
+    'https://sovj2weiosjke008php.herokuapp.com/proxy.php?url='.$url.'&ref='.$ref.'&name=pistolwayne003php&token=0e8635cf-e01e-4d5d-b778-53bb2ec48453'
 );
 $key = array_rand($arr, 1);
 //输出随机内容
