ba3213ef69ea5c1aee97877aa5e70c6f599ecb64
gdhdhdh1441414 <105533517+gdhdhdh1441414@users.noreply.github.com>
2023-03-03 23:43:25 +0800

    Update 正则预处理小文件.py

--- web/正则预处理小文件.py
+++ web/正则预处理小文件.py
@@ -45,10 +45,10 @@ for i, file in enumerate(files):
   return "文本已替换" + txt
 
 # 创建一个变量并存储我们要搜索的文本
- search_text = r".+?(\,\"duration\"\:.+?\,)\"thumbs\".+?\"640\"\:\"(.+?)\".+?(\"account\_type\"\:\".+?\")(\,\"name\"\:\")(.+?\").+?\"title\"\:\"(.+?)\"\,\"(share\_url\"\:\".+?\").*"
+ search_text = r"[\s\S]*\"title\":\"(.+?)\".+?(\"duration\"\:.+?\,).+?(\"share_url\"\:\".+?\").+?thumbs\".+?\"\:\"(.+?)\_.+?(\"name\"\:\")(.+?\").+?(\,\"account\_type\"\:\".+?\")[\s\S]*"
 
 #创建一个变量并存储我们要更新的文本
- replace_text = r'title>\6 from \5</title><br>\7\1\3\4\5<br><img src="\2?mw=240"  alt="img" /><br>'
+ replace_text = r'title>\1 from \6</title><br>\3\2\7\5\6<br><img src="\4?mw=240"  alt="img" /><br>'
 
 # 调用replacetext函数并打印返回的语句
  print(replacetext(search_text,replace_text))
