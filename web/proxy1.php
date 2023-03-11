8dcbb236a49d9190428be3e9f238e27b885c9914
gdhdhdh1441414 <105533517+gdhdhdh1441414@users.noreply.github.com>
2023-03-11 21:25:04 +0800

    Update proxy1.php

--- web/proxy1.php
+++ web/proxy1.php
@@ -87,7 +87,7 @@ $array = array(
  if (strstr($result, "avc_url")){
      
      
-  echo  'title>'.$title.'  from '.$author_name.'</title><br>"share_url":"'.$uri.'""duration":'.$duration.',"account_type":"'.$account_type.'","name":"'.$author_name.'<br><img src="'.$thumbnail_url.'?mw=240"  alt="img" >';
+  echo  'title>'.$title.'  from '.$author_name.'</title><br>"share_url":"'.$uri.'""duration":'.$duration.',"account_type":"'.$account_type.'","name":"'.$author_name.'"<br><img src="'.$thumbnail_url.'?mw=240"  alt="img" >';
 }else if (strstr($result, "this video cannot be played here") ){  //如果出现403
     
  
@@ -138,7 +138,7 @@ $thumbnail_url = $data['video']['thumbs']['base'];
 $uri = $data['video']['share_url']; 
 
 
-        echo  'title>'.$title.'  from '.$author_name.'</title><br>"share_url":"'.$uri.'""duration":'.$duration.',"account_type":"'.$account_type.'","name":"'.$author_name.'<br><img src="'.$thumbnail_url.'?mw=240"  alt="img" >';
+        echo  'title>'.$title.'  from '.$author_name.'</title><br>"share_url":"'.$uri.'""duration":'.$duration.',"account_type":"'.$account_type.'","name":"'.$author_name.'"<br><img src="'.$thumbnail_url.'?mw=240"  alt="img" >';
       break; 
    
     }
