33c88e575e7defc014653aec37bc07e3b2770719
gdhdhdh1441414 <105533517+gdhdhdh1441414@users.noreply.github.com>
2023-03-13 18:14:13 +0800

    Create 读取有效链接.py

---
+++ web/读取有效链接.py
@@ -0,0 +1,73 @@
+# -*- coding: UTF-8 -*-
+import concurrent.futures
+import requests
+import time
+
+urls = [
+    "https://vimeo362227.onrender.com",
+    "https://vimeo362227-1.onrender.com",
+    "https://vimeo362227-2.onrender.com",
+    "https://vimeo362227-3.onrender.com",
+    "https://vimeo362227-4.onrender.com",
+    "https://vimeo362227-5.onrender.com",
+    "https://vimeo362227-6.onrender.com",
+    "https://vimeo362227-7.onrender.com",
+    "https://vimeo362227-8.onrender.com",
+    "https://vimeo362227-9.onrender.com",
+    "https://vimeo362227-10.onrender.com",
+    "https://vimeo362227-11.onrender.com",
+    "https://vimeo362227-12.onrender.com",
+    "https://vimeo362227-13.onrender.com",
+    "https://vimeo362227-14.onrender.com",
+    "https://vimeo362227-15.onrender.com",
+    "https://vimeo362227-16.onrender.com",
+    "https://vimeo362227-17.onrender.com",
+    "https://vimeo362227-18.onrender.com",
+    "https://vimeo362227-19-7hgu.onrender.com",
+    "https://vimeo362227-20.onrender.com",
+    "https://vimeo362227-21.onrender.com",
+    "https://vimeo362227-22.onrender.com",
+    "https://vimeo362227-23.onrender.com",
+    "https://vimeo362227-24.onrender.com",
+    "https://vimeo362227-25.onrender.com",
+    "https://vimeo362227-26.onrender.com",
+    "https://vimeo362227-27.onrender.com",
+    "https://vimeo362227-28.onrender.com",
+    "https://vimeo362227-29.onrender.com",
+    "https://ellie001.onrender.com",
+    "https://ellie002.onrender.com",
+    "https://ellie003.onrender.com",
+    "https://ellie004.onrender.com",
+    "https://ellie006.onrender.com",
+    "https://ellie005.onrender.com",
+    "https://ellie010.onrender.com",
+    "https://ellie007.onrender.com",
+    "https://ellie008.onrender.com",
+    "https://ellie009.onrender.com",
+    "https://kai005.onrender.com",
+    "https://kai006.onrender.com"
+]
+
+successful_urls = []
+
+while True:
+    with concurrent.futures.ThreadPoolExecutor() as executor:
+        futures = []
+        for url in urls:
+            future = executor.submit(requests.get, url)
+            futures.append(future)
+
+        for future, url in zip(futures, urls):
+            response = future.result()
+            if response.status_code == 200:
+                print(f"{url} 请求成功！")
+                if url not in successful_urls:
+                    successful_urls.append(url)
+            else:
+                print(f"{url} 请求失败！")
+
+    with open('urls.txt', 'w') as f:
+        for url in successful_urls:
+            f.write(f"{url}\n")
+    print ("等待")
+    time.sleep(10)
