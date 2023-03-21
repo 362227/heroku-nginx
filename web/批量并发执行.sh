f4a5c9abefdf0d0414de826f7b84d8ea7a64810f
gdhdhdh1441414 <105533517+gdhdhdh1441414@users.noreply.github.com>
2023-03-21 18:12:43 +0800

    Update 批量并发执行.sh

--- web/批量并发执行.sh
+++ web/批量并发执行.sh
@@ -6,15 +6,24 @@ MAX_WORKERS=3
 # Define a function to upload a file
 function upload_file {
     file=$1
-    fake115uploader -e --oss-proxy http://127.0.0.1:1083 -c 2051423373191282165 -u "$file"; fake115uploader -e --oss-proxy http://127.0.0.1:1083 -c 2051423373191282165  -parts-num 37 -m "$file"
+    fake115uploader -e --oss-proxy http://127.0.0.1:1083 -c 2051423373191282165 -u "$file" || fake115uploader -e --oss-proxy http://127.0.0.1:1083 -c 2051423373191282165  -parts-num 37 -m "$file"
 }
 
 # Export the upload_file function so that it can be used by xargs
 export -f upload_file
 
+# Define a signal handler function to catch CTRL+C signal
+function sigint_handler {
+    echo "Interrupt signal received. Stopping the script."
+    exit 1
+}
+
+# Trap the CTRL+C signal and call the sigint_handler function
+trap sigint_handler INT
+
 # Use find to locate all files in /var/www/html/tx/
 # Pass each file to xargs to upload
 find /var/www/html/tx/ -type f -print0 | \
-    xargs -0 -P $MAX_WORKERS -I {} bash -c 'upload_file "$@"' _ {}
+    xargs -r -0 -P $MAX_WORKERS -I {} bash -c 'upload_file "$@"' _ {}
 
 echo "All commands have completed."
