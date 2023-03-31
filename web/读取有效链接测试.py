ce5de86d12ac70342e3d78ac938e43361fba8dc5
gdhdhdh1441414 <105533517+gdhdhdh1441414@users.noreply.github.com>
2023-03-31 13:14:19 +0800

    Update 读取有效链接测试.py

--- web/读取有效链接测试.py
+++ web/读取有效链接测试.py
@@ -6,8 +6,6 @@ from concurrent.futures import ThreadPoolExecutor, as_completed
 proxy = {'http': 'http://127.0.0.1:1086', 'https': 'http://127.0.0.1:1086'}
 
 # 需要尝试的链接列表
-
-
 urls = [
     "https://bcxdxrdqx-manyapps-001.onrender.com",
     "https://bcxdxrdqx-manyapps-002.onrender.com",
@@ -79,6 +77,7 @@ urls = [
     "https://resignation1-manyapps-016.onrender.com",
 ]
 
+
 while True:
     # 记录成功的链接
     successful_urls = []
@@ -88,19 +87,23 @@ while True:
         while True:
             try:
                 response = requests.get(url, timeout=20)
-                #response = requests.get(url, proxies=proxy, timeout=20)
                 if response.status_code == 200:
                     print(f'{url} returned 200')
                     successful_urls.append(url)
                     return None  # 返回None表示成功
                 else:
                     print(f'{url} returned {response.status_code}')
+                    if retry < 6:
+                        retry += 1
+                        print(f'{url} retrying {retry}/6')
+                    else:
+                        break
             except requests.exceptions.RequestException as e:
+                print(f'{url} failed: {e}')
                 if retry < 6:
                     retry += 1
-                    print(f'{url} failed: {e}, retrying {retry}/6')
+                    print(f'{url} retrying {retry}/6')
                 else:
-                    print(f'{url} failed: {e}, giving up')
                     break
             time.sleep(1)  # 等待1秒后重试
 
@@ -115,6 +118,11 @@ while True:
     with open('/mnt/d/常用/vimeo/传统方法刷-下载后再处理数据/urls.txt', 'w') as f:
         for url in successful_urls:
             f.write(url + '\n')
+
+    # 如果所有链接都成功，则退出循环
+    if set(successful_urls) == set(urls):
+        print("All URLs succeeded!")
+        break
 
     # 休眠一段时间后再次尝试
     print("一轮结束")
