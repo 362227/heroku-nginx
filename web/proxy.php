f396faa83b26a4ae4d77de938ef2e9d0203ef466
gdhdhdh1441414 <105533517+gdhdhdh1441414@users.noreply.github.com>
2023-04-03 15:11:47 +0800

    Update proxy.php

--- web/proxy.php
+++ web/proxy.php
@@ -1,8 +1,7 @@
 <?php
 // https://*****.onrender.com/vimeo.php， onrender下载这个proxy.php并重命名为vimeo.php
 $t1 = microtime(true);
-$t2 = microtime(true);
-//echo '程序耗时'.round($t2-$t1,3).'秒';
+
 
 ini_set('display_errors','off');
 date_default_timezone_set('PRC');
@@ -67,11 +66,9 @@ $array = array( "https://www.colorcollective.com",
 "http://www.pulsefilms.com", 
 "https://boyinthecastle.com", 
 "http://www.mathieuplainfosse.com", 
-"https://www.lutimedia.com", 
 "https://moxiepictures.com", 
 "https://www.els.tv", 
 "https://sesler.com", 
-"https://contrast.tv", 
 "https://www.finalcut-edit.com", 
 "https://the-quarry.co.uk",
 "https://therapystudios.com", 
@@ -79,10 +76,8 @@ $array = array( "https://www.colorcollective.com",
 "https://blackdogfilms.com",
 "http://malloybrothers.com/",
 "http://www.305films.com", 
-"https://www.company3.com", 
-"https://electrictheatre.tv", 
+ "https://electrictheatre.tv", 
 "http://www.benzene.paris/",
-"http://friendlondon.tv",
 "https://www.tenthree.co.uk",
 "https://alexanderhammer.com/",
 "https://samuelbayer.com/",
@@ -92,7 +87,7 @@ $array = array( "https://www.colorcollective.com",
 "https://makemakeentertainment.com",
 "http://www.jonasakerlund.com",
 "http://www.romanwhite.com",
-"https://www.pulsefilms.com",
+"http://000friendlondon.tv",
 "https://www.kaisaul.com",
 "https://coffeeand.tv/",
 "https://visionfilmco.com",
@@ -103,8 +98,10 @@ $array = array( "https://www.colorcollective.com",
 "https://www.lane-casting.com" ); 
 
 
+$array_chunked = array_chunk($array, ceil(count($array) / 2));
 
-
+$array1 = $array_chunked[0];
+$array2 = $array_chunked[1];
 
 
 
@@ -119,41 +116,23 @@ $array = array( "https://www.colorcollective.com",
   echo  'title>'.$title.'  from '.$author_name.'</title><br>"share_url":"'.$uri.'""duration":'.$duration.',"account_type":"'.$account_type.'","name":"'.$author_name.'"<br><img src="'.$thumbnail_url.'?mw=240"  alt="img" >';
 }else if (strstr($result, "this video cannot be played here") ){  //如果出现403
     
- 
-foreach ($array as $ref) { 
-
-
-$ch = curl_init();
-
-//curl_setopt($ch, CURLOPT_URL, 'https://vimeo.com/api/oembed.json?url=https%3A//vimeo.com/'.$id);
-curl_setopt($ch, CURLOPT_URL, 'https://player.vimeo.com/video/'.$id);
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'GET');
-
-curl_setopt($ch, CURLOPT_ENCODING, 'gzip, deflate');
-
-$headers = array();
-$headers[] = 'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9';
-$headers[] = 'Accept-Language: zh-CN,zh;q=0.9,en;q=0.8,ja;q=0.7,fr;q=0.6,ru;q=0.5';
-$headers[] = 'Cache-Control: max-age=0';
-$headers[] = 'Connection: keep-alive';
-$headers[] = 'Cookie: vuid=13se&geolocation=US%3BNV; OptanonAlertBoxClosed=2022-06-07T03:09:00.308Z; __cf_bm=hdM6BzJ5ZoUnhu38JlLdGoiOed1XjlOLkOO3D8pz8eU-1654582551-0-AaLvJIF3QJCD1RHRPrCjw7XOeDDwsScCg8Z9XKbm2TngJk=';
-$headers[] = 'Sec-Fetch-Dest: document';
-$headers[] = 'Sec-Fetch-Mode: navigate';
-$headers[] = 'Sec-Fetch-Site: none';
-$headers[] = 'Sec-Fetch-User: ?1';
-$headers[] = 'Referer:'.$ref;
-$headers[] = 'Upgrade-Insecure-Requests: 1';
-$headers[] = 'User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/102.0.5005.63 Safari/537.36';
-$headers[] = 'Sec-Ch-Ua: \" Not A;Brand\";v=\"99\", \"Chromium\";v=\"102\", \"Google Chrome\";v=\"102\"';
-$headers[] = 'Sec-Ch-Ua-Mobile: ?0';
-$headers[] = 'Sec-Ch-Ua-Platform: \"Windows\"';
-curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
     
-$result = curl_exec($ch);
+    
 
 
- 
+$loop_count = count($array) / 2;
+for ($i = 0; $i < $loop_count; $i++) {
+  $ref1 = $array1[$i];
+  $ref2 = $array2[$i];
+  $curl_cmd = "curl https://player.vimeo.com/video/$id --referer $ref1 & curl https://player.vimeo.com/video/$id --referer $ref2";
+  $result = shell_exec($curl_cmd);
+  
+
+    
+    
+    
+    
+    
     if (strstr($result, "avc_url")){
 
 $res = preg_replace('/[\s\S]*window.playerConfig \= |    var fullscreenSupported[\s\S]*/','', $result); //删除无效数据，提取json数据
@@ -188,8 +167,11 @@ else {  //如果不是403，也不包含avc_url，那就是其他的了，比如
     $result = str_replace($result, '1036222710362227103622271036222710362227103622271036222710362227103622271036222710362227103622271036222710362227', $result); //只要出现字符，就全部替换成10362227
 
     echo $result;}
-exit;
 
+$t2 = microtime(true);
+//echo '程序耗时'.round($t2-$t1,3).'秒';
+
+exit;
 //新方法新方法新方法新方法新方法新方法新方法新方法新方法新方法新方法
 
 
