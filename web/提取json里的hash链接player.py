d833d86f417a243ed69ac87b3119877a23a5b311
gdhdhdh1441414 <105533517+gdhdhdh1441414@users.noreply.github.com>
2023-04-04 01:51:54 +0800

    Add files via upload

---
+++ web/提取json里的hash链接player.py
@@ -0,0 +1,16 @@
+import re
+import glob
+import sys
+
+path = "sys.argv[1]"
+for filename in glob.glob(path):
+    with open(filename, encoding='gbk', errors='ignore') as f:
+        # 读入文件
+        file = f.read()
+
+        # 使用正则表达式匹配相应的内容
+        pattern = re.compile(r'"\\\/videos\\\/(?P<id>.+?)\:(?P<hash>.+?)\"')
+        matches = re.finditer(pattern, file)
+        
+        for m in matches:
+            print('https://crowncloud.362227.top/rss/刷vimeo跳转地址.php?url=https://player.vimeo.com/video/' + m.group('id') + "?h=" + m.group('hash') + '\n        out=' + m.group('id'))
