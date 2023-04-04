620b66250eeeb4eb5b6c4a995c36ba27c406bbb4
gdhdhdh1441414 <105533517+gdhdhdh1441414@users.noreply.github.com>
2023-04-04 10:56:01 +0800

    Update proxy1.php

--- web/proxy1.php
+++ web/proxy1.php
@@ -156,7 +156,7 @@ $uri = $data['video']['share_url'];
  
  
  else if (strstr($result, "This video does not exist.")) {
-     echo $id."为404为404为404为404为404为404"; 
+     echo "https://vimeo.com/api/oembed.json?url=https://vimeo.com/".$id;  //404不管三七二十一全部返回https://vimeo.com/api/oembed.json?url=https://vimeo.com/
 }
  
 else {  //如果不是403，也不包含avc_url，那就是其他的了，比如404、完全隐藏、有密码、真人验证、被封提示等等，这种情况下，验证码和被封提示被替换成空（便于被识别失败的链接），其他则替换成10362227
