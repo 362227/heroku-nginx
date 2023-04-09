10ee2ecab19502fdfe00df395fd203c28316a873
gdhdhdh1441414 <105533517+gdhdhdh1441414@users.noreply.github.com>
2023-04-09 21:16:09 +0800

    Create cloudshell-install.sh

---
+++ web/cloudshell-install.sh
@@ -0,0 +1,40 @@
+docker run -p 8080:80 dorowu/ubuntu-desktop-lxde-vnc
+
+sudo mkdir -p /mnt/d/
+sudo chmod a+rwx  -R /mnt/d
+sudo mkdir -p /mnt/d/常用/vimeo/传统方法刷-下载后再处理数据/ && sudo chmod a+rwx /mnt/d/常用/vimeo/传统方法刷-下载后再处理数据
+sudo mkdir -p /mnt/data/aria2_downloads && sudo chmod a+rwx /mnt/data/aria2_downloads
+sudo  mkdir -p /var/www/html/tx && sudo chmod a+rwx  /var/www/html/tx
+
+sudo wget -O /usr/local/bin/BaiduPCS-Go "https://github.com/362227/kod/raw/master/web/data/BaiduPCS-Go"
+sudo chmod +x /usr/local/bin/BaiduPCS-Go
+
+sudo wget -O /usr/local/bin/fake115uploader "https://raw.githubusercontent.com/362227/kod/master/web/data/fake115uploader"
+sudo chmod +x /usr/local/bin/fake115uploader
+
+sudo wget -O /usr/local/bin/fake115uploader.json "https://362227.top/fake115uploader.json"
+sudo chmod +x /usr/local/bin/fake115uploader.json
+
+sudo wget -O /usr/local/bin/v2ray  "https://github.com/362227/kod/raw/master/web/data/v2ray"
+sudo chmod +x /usr/local/bin/v2ray 
+
+sudo wget -O /usr/local/bin/proxychains "https://github.com/gdhdhdh1441414/Remote-Uploader-HEROKU/raw/main/proxychains"
+sudo chmod +x /usr/local/bin/proxychains
+
+sudo wget -O /usr/local/bin/heroku.json "https://github.com/362227/kod/raw/master/web/data/heroku.json"
+
+sudo wget -O /mnt/d/常用/vimeo/传统方法刷-下载后再处理数据/读取有效链接.py "https://github.com/gdhdhdh1441414/heroku-nginx-php-tor/raw/main/web/%E8%AF%BB%E5%8F%96%E6%9C%89%E6%95%88%E9%93%BE%E6%8E%A5.py"
+sudo wget -O /mnt/d/常用/vimeo/传统方法刷-下载后再处理数据/正则预处理小文件.py "https://github.com/gdhdhdh1441414/heroku-nginx-php-tor/raw/main/web/%E6%AD%A3%E5%88%99%E9%A2%84%E5%A4%84%E7%90%86%E5%B0%8F%E6%96%87%E4%BB%B6.py"
+sudo wget -O /mnt/d/常用/vimeo/传统方法刷-下载后再处理数据/正则预处理.py "https://github.com/gdhdhdh1441414/heroku-nginx-php-tor/raw/main/web/%E6%AD%A3%E5%88%99%E9%A2%84%E5%A4%84%E7%90%86.py"
+sudo wget -O /mnt/d/常用/vimeo/传统方法刷-下载后再处理数据/链接.py "https://raw.githubusercontent.com/gdhdhdh1441414/heroku-nginx-php-tor/main/web/%E9%93%BE%E6%8E%A5.py"
+sudo wget -O /mnt/d/常用/vimeo/传统方法刷-下载后再处理数据/从log文件提取有ref的链接.py "https://github.com/gdhdhdh1441414/heroku-nginx-php-tor/raw/main/web/%E4%BB%8Elog%E6%96%87%E4%BB%B6%E6%8F%90%E5%8F%96%E6%9C%89ref%E7%9A%84%E9%93%BE%E6%8E%A5.py"
+sudo wget -O /mnt/d/常用/vimeo/传统方法刷-下载后再处理数据/正则提取最新.py "https://github.com/gdhdhdh1441414/heroku-nginx-php-tor/raw/main/web/%E6%AD%A3%E5%88%99%E6%8F%90%E5%8F%96%E6%9C%80%E6%96%B0.py"
+sudo wget -O /mnt/d/常用/vimeo/传统方法刷-下载后再处理数据/从log文件提取下载失败的链接.py https://github.com/gdhdhdh1441414/heroku-nginx-php-tor/raw/main/web/%E4%BB%8Elog%E6%96%87%E4%BB%B6%E6%8F%90%E5%8F%96%E4%B8%8B%E8%BD%BD%E5%A4%B1%E8%B4%A5%E7%9A%84%E9%93%BE%E6%8E%A5.py
+sudo wget -O /mnt/d/常用/vimeo/传统方法刷-下载后再处理数据/替换为onrender链接.py "https://github.com/gdhdhdh1441414/heroku-nginx-php-tor/raw/main/web/%E6%9B%BF%E6%8D%A2%E4%B8%BAonrender%E9%93%BE%E6%8E%A5.py"
+ 
+sudo wget -O /mnt/d/常用/vimeo/传统方法刷-下载后再处理数据/替换为onrender链接.py https://github.com/gdhdhdh1441414/heroku-nginx-php-tor/raw/main/web/%E6%9B%BF%E6%8D%A2%E4%B8%BAonrender%E9%93%BE%E6%8E%A5.py
+
+sudo apt install mediainfo -y
+
+sudo wget -O /mnt/d/常用/vimeo/传统方法刷-下载后再处理数据/链接01.txt https://github.com/gdhdhdh1441414/heroku-nginx-php-tor/releases/download/file/01.txt
+sudo wget -O /mnt/d/常用/vimeo/传统方法刷-下载后再处理数据/链接02.txt https://github.com/gdhdhdh1441414/heroku-nginx-php-tor/releases/download/file2/02.txt
