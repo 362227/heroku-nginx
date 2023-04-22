02bbbedc45889ae97cb58c0ebb05076e46c4c191
gdhdhdh1441414 <105533517+gdhdhdh1441414@users.noreply.github.com>
2023-04-22 11:57:16 +0800

    Update 读取有效链接hash.py

--- web/读取有效链接hash.py
+++ web/读取有效链接hash.py
@@ -234,10 +234,12 @@ while True:
             pass
 
     # 将成功的链接写入文件
-    if len(successful_urls) >= 120:
+    if len(successful_urls) >= 140:
         with open('/mnt/d/常用/vimeo/传统方法刷-下载后再处理数据/urls.txt', 'w') as f:
             for url in successful_urls:
                 f.write(url + '\n')
+        print("以下链接未成功写入urls.txt:")
+        print(set(urls) - set(successful_urls))
     else:
         print('Successful URLs less than 40, skipped writing to file.')
 
