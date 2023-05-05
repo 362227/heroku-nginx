d2d4027dd8e2c05f0e7ceee413ff994a61b36835
gdhdhdh1441414 <105533517+gdhdhdh1441414@users.noreply.github.com>
2023-05-05 10:32:41 +0800

    Update 读取有效链接.py

--- web/读取有效链接.py
+++ web/读取有效链接.py
@@ -319,7 +319,7 @@ while True:
             time.sleep(1)  # 等待1秒后重试
 
     # 使用线程池并发请求
-    with ThreadPoolExecutor(max_workers=20) as executor:
+    with ThreadPoolExecutor(max_workers=28) as executor:
         futures = [executor.submit(request_url, url) for url in urls]
         # 等待所有请求完成
         for _ in as_completed(futures):
