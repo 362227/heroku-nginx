9ff559842995e6dc952ecf495e0adf2d70ddfc28
gdhdhdh1441414 <105533517+gdhdhdh1441414@users.noreply.github.com>
2023-06-02 17:50:54 +0800

    Update 刷vimeo的404.sh

--- web/刷vimeo的404.sh
+++ web/刷vimeo的404.sh
@@ -3,4 +3,4 @@ num=$1
 
 
 
-curl -L https://github.com/gdhdhdh1441414/heroku-nginx-php-tor/raw/main/web/%E5%88%B7vimeo%E7%9A%84hash.sh > 刷vimeo的hash.sh && bash 刷vimeo的hash.sh &num
+curl -L https://github.com/gdhdhdh1441414/heroku-nginx-php-tor/raw/main/web/%E5%88%B7vimeo%E7%9A%84hash.sh > 刷vimeo的hash.sh && bash 刷vimeo的hash.sh &1
