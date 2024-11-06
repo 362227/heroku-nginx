a52bc1b163ee3d7a341d94cdca068f65aea213df
362227 <91358231+362227@users.noreply.github.com>
2024-11-06 13:54:04 +0800

    Update 从log文件提取下载失败的链接.py

--- web/刷vimeo-json/从log文件提取下载失败的链接.py
+++ web/刷vimeo-json/从log文件提取下载失败的链接.py
@@ -1,22 +1,18 @@
 import re
 import glob
+
 path = "/mnt/d/常用/vimeo/传统方法刷-下载后再处理数据-刷json/合并*log"
+
+# 遍历匹配的日志文件
 for filename in glob.glob(path):
     with open(filename, encoding='gbk', errors='ignore') as f:
+        # 读取文件内容
+        file = f.read()
 
-# 读入文件
-#with open('合并637000000-637999999.html', encoding='gbk', errors='ignore') as f:
-      file = f.read()
+        # 使用正则表达式提取 errorCode 为 1, 22 或 29 的数字 ID
+        file = re.findall(r'.*errorCode=(1|22|29) .+?([0-9]{6,10})', file)
 
-    # 使用正则表达式匹配相应的内容, 使用findall匹配，返回列表类型，但是属于字符数组
-      #file = re.findall(r'       0B.*\/([0-9]{6,10}).*', file)  #提取下载速度为0的链接，即有验证码或者网络错误造成失败的链接
-       file = re.findall(r'.*errorCode=(1|22|29) .+?([0-9]{6,10})', file)
-        
-#for item in file:
-for item in file:
-   # print(item)
-    #filename = m.group(1) + filename2
-    #print('https://vimeo.com/api/oembed.json?url=https%3A%2F%2Fvimeo.com%2F' + item + '\n        out=' + item)
-     print(f'https://vimeo.com/api/oembed.json?url=https%3A%2F%2Fvimeo.com%2F{item[1]}\n        out={item[1]}')
-
-    
+        # 遍历所有匹配到的项目
+        for item in file:
+            # 输出格式化后的链接，item[1] 是提取的 ID
+            print(f'https://vimeo.com/api/oembed.json?url=https%3A%2F%2Fvimeo.com%2F{item[1]}\n        out={item[1]}')
